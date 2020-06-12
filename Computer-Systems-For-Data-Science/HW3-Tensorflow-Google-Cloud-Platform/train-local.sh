6eb3241a5429a68ef89b90b634503136a7a50980
@maxime.tchibozo <maxime.tchibozo@telecom-paristech.fr>
2020-06-11 21:57:33 -0400

    Base code for HW3. We had to modify some functions and train models on the cloud.

---
+++ Computer-Systems-For-Data-Science/HW3-Tensorflow-Google-Cloud-Platform/train-local.sh
@@ -0,0 +1,4 @@
+python cifar10_main.py --data-dir=${PWD}/cifar-10-data \
+                       --job-dir=/tmp/cifar10 \
+                       --num-gpus=0 \
+                       --train-steps=1000
